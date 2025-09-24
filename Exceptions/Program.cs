209ccd70f8dc578d075c16cf6df515dedb21c4cd
Luis Coco Enríquez <LEnriquez@luxoft.com>
2025-09-24 13:35:59 +0200

    adding Exceptions sample project

---
+++ Exceptions/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        string input = Console.ReadLine();
+
+        try
+        {
+            int n = int.Parse(input);
+            Console.WriteLine("You entered: " + n);
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("Error: Invalid number format.");
+        }
+        finally
+        {
+            Console.WriteLine("Finally block: cleanup code runs here.");
+        }
+    }
+}
+
